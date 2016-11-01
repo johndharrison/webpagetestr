5e257d146fdf0e67d40574b2496668c2fc074832
johndharrison <johndharrison0@gmail.com>
2016-11-01 12:40:10 -0700

    Add assertions

--- R/assertions.R
+++ R/assertions.R
@@ -1,13 +1,23 @@
-assert_server <- function(x) {
-  stopifnot(
-    inherits(x, "WPTserver")
-  )
+is_server <- function(x) {
+  inherits(x, "WPTserver")
+}
+
+assertthat::on_failure(is_server) <-  function(call, env) {
+  paste0(sub("caArgs\\$", "", deparse(call$x)), " is not a WPTserver")
 }
 
 is_string <- function(x) {
   is.character(x) && length(x) == 1 && !is.na(x)
 }
 
-assert_string_or_null <- function(x) {
-  stopifnot(is_string(x) || is.null(x))
+assertthat::on_failure(is_string) <-  function(call, env) {
+  paste0(sub("caArgs\\$", "", deparse(call$x)), " is not a string")
+}
+
+is_string_or_null <- function(x) {
+  is_string(x) || is.null(x)
+}
+
+assertthat::on_failure(is_string_or_null) <-  function(call, env) {
+  paste0(sub("caArgs\\$", "", deparse(call$x)), " is not a string or null")
 }
