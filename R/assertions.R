e7863c5454a6517b28213dd0fc98f5cddb651ffd
johndharrison <johndharrison0@gmail.com>
2016-11-01 11:09:42 -0700

    Add assertions

---
+++ R/assertions.R
@@ -0,0 +1,13 @@
+assert_server <- function(x) {
+  stopifnot(
+    inherits(x, "WPTserver")
+  )
+}
+
+is_string <- function(x) {
+  is.character(x) && length(x) == 1 && !is.na(x)
+}
+
+assert_string_or_null <- function(x) {
+  stopifnot(is_string(x) || is.null(x))
+}
