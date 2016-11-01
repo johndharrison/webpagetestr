598e0f73c996b0fdf7b497005326ccf85acda363
johndharrison <johndharrison0@gmail.com>
2016-11-01 14:58:43 -0700

    Add content to getLocations

--- R/getLocations.R
+++ R/getLocations.R
@@ -6,12 +6,25 @@
 #' @template  requestId 
 #' @template ellipsis
 #'
-#' @return
+#' @return A list of locations and the number of pending tests at those locations
 #' @export
 #'
-#' @examples
+#' @example /inst/examples/docs/getLocations.R
 
 getLocations <- function(server, requestId = NULL, ...){
-   checkArgs(server, requestId)
+  checkArgs(server, requestId)
+  body <- setNames(
+    list(requestId, "json"),
+    c(WPTOptions$requestId$api, "f")
+  )
+  res <- sendQuery(url = file.path(server$url, WPTPaths$locations), 
+                   body = body, origin = "getLocations", ...)
+  locs <- content(res)
+  if(identical(locs$statusCode, 200L)){
+    `attr<-`(locs$data, "class", "locations")
+  }else{
+    stop("Server returned a statusCode = ", locs$statusCode,
+         " with statusText = ", locs$statusText)
+  }
 } 
 
