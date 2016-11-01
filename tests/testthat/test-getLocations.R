693b99bfe5c1503728ef4a178c9df3bf1c934c9a
johndharrison <johndharrison0@gmail.com>
2016-11-01 16:35:15 -0700

    Add test for getLocations mocking a 400 response.

--- tests/testthat/test-getLocations.R
+++ tests/testthat/test-getLocations.R
@@ -5,3 +5,17 @@ test_that("canGetLocations", {
   locs <- getLocations(WPT)  
   expect_s3_class(locs, "locations")
 })
+
+test_that("canGetErrorFromGetLocations", {
+  WPT <- server()
+  with_mock(
+    `httr::POST` = function(...){
+      list(statusCode = 400,
+           statusText = "Some location error")},
+    `httr::stop_for_status` = function(...){}, 
+    `httr::content` = function(x){x},
+    expect_error(
+      locs <- getLocations(WPT), "Server returned a statusCode = 400"   
+    )
+  )
+})
