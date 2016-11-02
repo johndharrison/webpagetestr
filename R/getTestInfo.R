2cf3ef5da5342b945feb76727705c419b62f7574
johndharrison <johndharrison0@gmail.com>
2016-11-01 19:31:54 -0700

    Add getTestInfo function and docs

--- R/getTestInfo.R
+++ R/getTestInfo.R
@@ -1,3 +1,32 @@
-getTestInfo <- function(){
-} 
+#' Get test info
+#'
+#' get test request info/details
+#'
+#' @template server 
+#' @template testId
+#' @template  dryRun 
+#' @template ellipsis
+#'
+#' @return returns info on a test
+#' @export
+#'
+#' @example /inst/examples/docs/getTestInfo.R
 
+getTestInfo <- function(server, testId , dryRun = FALSE, ...){
+  checkArgs(server, testId, dryRun)
+  body <- setNames(
+    list(testId, "json"),
+    c("test", "f")
+  )
+  res <- sendQuery(url = file.path(server$url, WPTPaths$testStatus), 
+                   body = body, origin = "getTestStatus", dryRun = dryRun,
+                   ...)
+  if(dryRun){return(res)}
+  testinfo <- content(res)
+  if(identical(testinfo$statusCode, 200L)){
+    `attr<-`(testinfo$data, "class", "testinfo")
+  }else{
+    stop("Server returned a statusCode = ", testinfo$statusCode,
+         " with statusText = ", testinfo$statusText)
+  }
+} 
---
+++ inst/examples/docs/getTestInfo.R
@@ -0,0 +1,4 @@
+\dontrun{
+  WPT <- server()
+  testinfo <- getTestInfo(WPT, testId = "someid")  
+}
