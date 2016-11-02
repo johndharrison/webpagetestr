d757b9b107abb78fed9b1b877ccd37e8452356b2
johndharrison <johndharrison0@gmail.com>
2016-11-01 18:31:42 -0700

    Add getTesters function, docs

--- R/getTesters.R
+++ R/getTesters.R
@@ -1,3 +1,33 @@
-getTesters <- function(){
+#' Get testers status
+#'
+#' list testers status and details
+#' 
+#' @template server 
+#' @template requestId 
+#' @template dryrun 
+#' @template ellipsis 
+#'
+#' @return A list of testers status and details
+#' @export
+#'
+#' @example /inst/examples/docs/getTesters.R
+
+getTesters <- function(server, requestId = NULL, dryrun = FALSE, ...){
+  checkArgs(server, requestId, dryrun)
+  body <- setNames(
+    list(requestId, "json"),
+    c(WPTOptions$requestId$api, "f")
+  )
+  res <- sendQuery(url = file.path(server$url, WPTPaths$testers), 
+                   body = body, origin = "getTesters", dryrun = dryrun,
+                   ...)
+  if(dryrun){return(res)}
+  testers <- content(res)
+  if(identical(testers$statusCode, 200L)){
+    `attr<-`(testers$data, "class", "locations")
+  }else{
+    stop("Server returned a statusCode = ", testers$statusCode,
+         " with statusText = ", testers$statusText)
+  }
 } 
 
