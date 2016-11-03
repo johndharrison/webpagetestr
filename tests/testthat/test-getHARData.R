ee6716f54d4eb48bd8ad89c0ec6ac7bf22770761
johndharrison <johndharrison0@gmail.com>
2016-11-02 19:39:04 -0700

    Add getHARData dryRun test

--- tests/testthat/test-getHARData.R
+++ tests/testthat/test-getHARData.R
@@ -7,3 +7,12 @@ test_that("canGetHARData", {
   har <- getHARData(WPT, tID)
   expect_s3_class(har, "har")
 })
+
+test_that("canGetHARDataDryRun", {
+  WPT <- server()
+  har <- getHARData(WPT, "someID", dryRun = TRUE)
+  expect_identical(
+    har, 
+    paste0(WPT$url, "/export.php?test=someID&f=json")
+  )
+})
