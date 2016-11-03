885a0772206e0a9ef488e43cfe6909ff6e6d01d7
johndharrison <johndharrison0@gmail.com>
2016-11-03 07:21:36 -0700

    Add getUtilizationData function, docs and tests

--- R/getUtilizationData.R
+++ R/getUtilizationData.R
@@ -1,3 +1,42 @@
-getUtilizationData <- function(){
-} 
+#' Get utilisation data
+#'
+#' get the CPU, bandwidth and memory utilisation data from test
+#' 
+#' @template  server 
+#' @template  testId 
+#' @template  run 
+#' @template  repeatView 
+#' @template  dryRun 
+#' @template ellipsis
+#'
+#' @return A tibble of the CPU, bandwidth and memory utilisation data 
+#'    from test
+#' @export
+#'
+#' @example /inst/examples/docs/getUtilizationData.R
 
+getUtilizationData <- function(server, testId, run = 1L, repeatView = FALSE, 
+                             dryRun = FALSE, ...){
+  checkArgs(server, run, dryRun)
+  psfile <- sprintf("%s%s_%s", run, 
+                    ifelse(repeatView, WPTfileNames$cached, ""), 
+                    WPTfileNames$utilization)
+  body <- list(
+    list(testId ,"test"),
+    list(psfile , "file")
+  )
+  body <- setNames(lapply(body, "[[", 1), 
+                   vapply(body, "[[", character(1), 2))
+  res <- sendQuery(url = server$url, path = WPTPaths$gzip, body = body, 
+                   origin = "getUtilizationData", dryRun = dryRun,
+                   method = "GET", ...)
+  if(dryRun){return(res)}
+  utilization <- content(res, type = "text/csv", encoding = "UTF-8", 
+                         col_types = readr::cols(
+    "Offset Time (ms)" = "i",
+    "Bandwidth In (kbps)" = "i",
+    "CPU Utilization (%)" = "n",
+    "Memory Use (KB)" = "i"
+  ))
+  utilization
+} 
---
+++ tests/testthat/test-getUtilizationData.R
@@ -0,0 +1,29 @@
+context("getUtilizationData")
+
+test_that("cangetUtilizationDataDryRun", {
+  WPT <- server()
+  utilizationData <- getUtilizationData(WPT, testId = "atestid", run = 2L, 
+                             repeatView = TRUE, dryRun = TRUE)  
+  expect_identical(
+    utilizationData, 
+    paste0(WPT$url, 
+           "/getgzip.php?test=atestid&file=2_Cached_progress.csv")
+  )
+})
+
+test_that("canGetErrorFromgetUtilizationData", {
+  WPT <- server()
+  expect_error(
+    getUtilizationData(WPT, testId = "atestid"), 
+    "getUtilizationData returned response: 404"   
+  )
+})
+
+test_that("cangetUtilizationData", {
+  WPT <- server()
+  history <- getHistory(WPT, filter = "London")
+  tID <- as.character(history[dim(history)[1], "Test ID"])
+  utilizationData <- getUtilizationData(WPT, tID)
+  expect_s3_class(utilizationData, "tbl_df")
+})
+
