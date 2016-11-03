7faf809aa92c7efafa610a705552961912398359
johndharrison <johndharrison0@gmail.com>
2016-11-02 17:19:43 -0700

    Fix typo

--- inst/examples/docs/getHARData.R
+++ inst/examples/docs/getHARData.R
@@ -1,6 +1,6 @@
 \dontrun{
   WPT <- server()
   history <- getHistory(WPT, filter = "London")
-  tID <- as.character(out[dim(out)[1], "Test ID"])
+  tID <- as.character(history[dim(history)[1], "Test ID"])
   har <- getHARData(WPT, tID)
 }
--- tests/testthat/test-getHARData.R
+++ tests/testthat/test-getHARData.R
@@ -3,7 +3,7 @@ context("getHARData")
 test_that("canGetHARData", {
   WPT <- server()
   history <- getHistory(WPT, filter = "London")
-  tID <- as.character(out[dim(out)[1], "Test ID"])
+  tID <- as.character(history[dim(history)[1], "Test ID"])
   har <- getHARData(WPT, tID)
   expect_s3_class(har, "har")
 })
