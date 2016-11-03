715f5541055f946bd57cfd2005ba40b9908a279b
johndharrison <johndharrison0@gmail.com>
2016-11-03 10:10:19 -0700

    Add getTimelineData function, docs and tests

--- R/getTimelineData.R
+++ R/getTimelineData.R
@@ -1,3 +1,33 @@
-getTimelineData <- function(){
-} 
+#' Get timeline data (chrome)
+#'
+#' get the Chrome Developer Tools Timeline data (if available) from test
+#' 
+#' @template  server 
+#' @template  testId 
+#' @template  run 
+#' @template  repeatView 
+#' @template  dryRun 
+#' @template ellipsis
+#'
+#' @return A list containing the timeline
+#' @export
+#'
+#' @example /inst/examples/docs/getTimelineData.R
 
+getTimelineData <- function(server, testId, run = 1L, repeatView = FALSE, 
+                           dryRun = FALSE, ...){
+  checkArgs(server, run, dryRun)
+  body <- list(
+    list(testId ,"test"),
+    list(run , "run"),
+    list(false2Null(repeatView) , "cached")
+  )
+  body <- setNames(lapply(body, "[[", 1), 
+                   vapply(body, "[[", character(1), 2))
+  res <- sendQuery(url = server$url, path = WPTPaths$timeline, body = body, 
+                   origin = "getTimelineData", dryRun = dryRun,
+                   method = "GET", ...)
+  if(dryRun){return(res)}
+  timelinedata <- content(res)
+  timelinedata
+} 
---
+++ tests/testthat/test-getTimelineData.R
@@ -0,0 +1,48 @@
+context("getTimelineData")
+
+test_that("canGetTimelineDataDryRun", {
+  WPT <- server()
+  timelineData <- getTimelineData(WPT, testId = "atestid", run = 2L, 
+                                  repeatView = TRUE, dryRun = TRUE)  
+  expect_identical(
+    timelineData, 
+    paste0(WPT$url, "/getTimeline.php?test=atestid&run=2&cached=1")
+  )
+})
+
+test_that("canGetErrorFromgetTimelineData", {
+  WPT <- server()
+  expect_error(
+    getTimelineData(WPT, testId = "atestid"), 
+    "getTimelineData returned response: 404"   
+  )
+})
+
+test_that("canGetTestInfoViaMock", {
+  WPT <- server()
+  with_mock(
+    `httr::POST` = function(...){
+      list(statusCode = 200L,
+           statusText = "Some test info message",
+           data = list(hello = "world"))},
+    `httr::stop_for_status` = function(...){}, 
+    `httr::content` = function(x){x},
+    expect_s3_class(
+      getTestInfo(WPT, testId = "atestid"), 
+      "testinfo"   
+    )
+  )
+})
+test_that("canGetTimelineDataViaMock", {
+  WPT <- server()
+  with_mock(
+    `httr::GET` = function(...){
+      list(statusCode = 200L,
+           statusText = "Some test info message",
+           data = list(hello = "world"))},
+    `httr::stop_for_status` = function(...){}, 
+    `httr::content` = function(x){x},
+    expect_is(getTimelineData(WPT, "atestid"), "list")
+  )
+})
+
