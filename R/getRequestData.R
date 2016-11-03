7ef311a64b68d75119f8b9546bc7f903eac45b42
johndharrison <johndharrison0@gmail.com>
2016-11-03 09:28:42 -0700

    Add getRequestData function, docs and tests

--- R/getRequestData.R
+++ R/getRequestData.R
@@ -1,3 +1,36 @@
-getRequestData <- function(){
-} 
+#' Get request data
+#'
+#' get the request data from test
+#' 
+#' @template  server 
+#' @template  testId 
+#' @template  run 
+#' @template  repeatView 
+#' @template  dryRun 
+#' @template ellipsis
+#'
+#' @return A tibble containing the request data
+#' @export
+#'
+#' @example /inst/examples/docs/getRequestData.R
 
+getRequestData <- function(server, testId, run = 1L, repeatView = FALSE, 
+                             dryRun = FALSE, ...){
+  checkArgs(server, run, dryRun)
+  psfile <- sprintf("%s%s_%s", run, 
+                    ifelse(repeatView, WPTfileNames$cached, ""), 
+                    WPTfileNames$request)
+  body <- list(
+    list(testId ,"test"),
+    list(psfile , "file")
+  )
+  body <- setNames(lapply(body, "[[", 1), 
+                   vapply(body, "[[", character(1), 2))
+  res <- sendQuery(url = server$url, path = WPTPaths$gzip, body = body, 
+                   origin = "getRequestData", dryRun = dryRun,
+                   method = "GET", ...)
+  if(dryRun){return(res)}
+  requestdata <- 
+    parseRequestData(content(res, as = "text", encoding = "UTF-8"))
+  requestdata
+} 
---
+++ tests/testthat/test-getRequestData.R
@@ -0,0 +1,29 @@
+context("getRequestData")
+
+test_that("cangetRequestDataDryRun", {
+  WPT <- server()
+  requestData <- getRequestData(WPT, testId = "atestid", run = 2L, 
+                             repeatView = TRUE, dryRun = TRUE)  
+  expect_identical(
+    requestData, 
+    paste0(WPT$url, 
+           "/getgzip.php?test=atestid&file=2_Cached_IEWTR.txt")
+  )
+})
+
+test_that("canGetErrorFromgetRequestData", {
+  WPT <- server()
+  expect_error(
+    getRequestData(WPT, testId = "atestid"), 
+    "getRequestData returned response: 404"   
+  )
+})
+
+test_that("cangetRequestData", {
+  WPT <- server()
+  history <- getHistory(WPT, filter = "London")
+  tID <- as.character(history[dim(history)[1], "Test ID"])
+  requestData <- getRequestData(WPT, tID)
+  expect_s3_class(requestData, "tbl_df")
+})
+
