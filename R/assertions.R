361cb6e98cbf6edc214629bdf23363ff285bb4ce
johndharrison <johndharrison0@gmail.com>
2016-11-07 06:24:57 -0800

    Ammend assertions

--- R/assertions.R
+++ R/assertions.R
@@ -1,12 +1,9 @@
-caArgsReg <- "caArgs\\[\\[\"(.*)\"\\]\\]"
-
 is_server <- function(x) {
   inherits(x, "WPTserver")
 }
 
 assertthat::on_failure(is_server) <-  function(call, env) {
-  paste0(sub(caArgsReg, "\\1", deparse(call[["x"]])), 
-         " is not a WPTserver")
+  paste0(env$x, " is not a WPTserver")
 }
 
 is_string <- function(x) {
@@ -14,7 +11,7 @@ is_string <- function(x) {
 }
 
 assertthat::on_failure(is_string) <-  function(call, env) {
-  paste0(sub(caArgsReg, "\\1", deparse(call[["x"]])), " is not a string")
+  paste0(env$x, " is not a string")
 }
 
 is_string_or_null <- function(x) {
@@ -22,8 +19,7 @@ is_string_or_null <- function(x) {
 }
 
 assertthat::on_failure(is_string_or_null) <-  function(call, env) {
-  paste0(sub(caArgsReg, "\\1", deparse(call[["x"]])), 
-         " is not a string or null")
+  paste0(env$x, " is not a string or null")
 }
 
 is_logical <- function(x){
@@ -31,8 +27,7 @@ is_logical <- function(x){
 }
 
 assertthat::on_failure(is_logical) <-  function(call, env) {
-  paste0(sub(caArgsReg, "\\1", deparse(call[["x"]])), 
-         " is not of type logical")
+  paste0(env$x, " is not of type logical")
 }
 
 is_integer_or_null <- function(x) {
@@ -40,8 +35,7 @@ is_integer_or_null <- function(x) {
 }
 
 assertthat::on_failure(is_integer_or_null) <-  function(call, env) {
-  paste0(sub(caArgsReg, "\\1", deparse(call[["x"]])), 
-         " is not an integer or null")
+  paste0(env$x, " is not an integer or null")
 }
 
 is_integer <- function(x) {
@@ -49,7 +43,7 @@ is_integer <- function(x) {
 }
 
 assertthat::on_failure(is_integer) <-  function(call, env) {
-  paste0(sub(caArgsReg, "\\1", deparse(call[["x"]])), " is not an integer")
+  paste0(env$x, " is not an integer")
 }
 
 is_writeable_path <- function(x){
@@ -58,8 +52,7 @@ is_writeable_path <- function(x){
 }
 
 assertthat::on_failure(is_writeable_path) <-  function(call, env) {
-  paste0(sub(caArgsReg, "\\1", deparse(call[["x"]])), 
-         " is not a writeable file/path")
+  paste0(env$x, " is not a writeable file/path")
 }
 
 is_character <- function(x) {
@@ -67,8 +60,7 @@ is_character <- function(x) {
 }
 
 assertthat::on_failure(is_character) <-  function(call, env) {
-  paste0(sub(caArgsReg, "\\1", deparse(call[["x"]])), 
-         " is not a character vector")
+  paste0(env$x, " is not a character vector")
 }
 
 is_character_or_null <- function(x) {
@@ -76,7 +68,6 @@ is_character_or_null <- function(x) {
 }
 
 assertthat::on_failure(is_character_or_null) <-  function(call, env) {
-  paste0(sub(caArgsReg, "\\1", deparse(call[["x"]])), 
-         " is not a character vector or NULL")
+  paste0(env$x, " is not a character vector or NULL")
 }
 
